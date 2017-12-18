b0b284b5b790bd9917ffa5f38a80b31e9c27f335
Willian <willian.agostini94@gmail.com>
2017-12-18 15:53:54 -0200

    alterado a dependencia do bootstrap, laterado do gerenciador de pacotes para os links em cdn

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,15 @@
 import { BrowserModule } from "@angular/platform-browser";
-import { NgModule, ComponentFactoryResolver, ElementRef } from "@angular/core";
+import { NgModule  } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { TabComponent } from "./tab/tab.component";
 import { TabService } from "./tab/tab.service";
-import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 import { TeacherComponent } from "./teacher/teacher.component";
 import { StudentComponent } from "./student/student.component";
 import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [TabComponent, StudentComponent, TeacherComponent, HomeComponent],
-  imports: [BrowserModule, CommonModule, NgbModule.forRoot()],
+  imports: [BrowserModule, CommonModule],
   providers: [TabService],
   entryComponents: [StudentComponent, TeacherComponent],
   bootstrap: [HomeComponent]
